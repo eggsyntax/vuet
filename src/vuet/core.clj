3d238f371d53e3ad7527b13cb97ed86383855e7e
Egg Syntax <egg@novonon.com>
2015-04-28 15:40:17 -0400

    Get undo working

--- src/vuet/core.clj
+++ src/vuet/core.clj
@@ -1,13 +1,14 @@
 (ns vuet.core
   (:use [clojure.pprint :only (pprint)]
+        [clojure.tools.trace]
         [clojure.string :only (join)])
-  (:require [clojure.zip :as z]
-            [clojure.tools.namespace.repl :as repl])
+  (:require [clojure.zip :as z])
   (:import jline.Terminal)
   (:gen-class))
 
 (def char-range (seq "abcdefghijklmnopqrstuwvxyz")) ;TODO
 (def index (atom 0))
+(def history (atom '()))
 
 (defn next-index []
   (swap! index inc))
@@ -43,7 +44,7 @@
     (add-fn loc (next-index))
     (println "\nCan't add a node from a leaf position")))
 
-(defn insert [loc]
+(deftrace insert [loc]
   (add-node z/insert-child loc))
 
 (defn append [loc]
@@ -53,6 +54,36 @@
   (if (z/end? (z/next loc))
     (println "Can't usefully continue.")
     (z/next loc)))
+
+(defn print-history [& args]
+  (println)
+  (println "History:")
+  (dorun (map println (reverse @history)))
+  (println))
+
+(defn -drop-and-return
+  "Given an atom containing a list, strip the first item,
+  store the shortened version, and return the stripped item"
+  [list-atom]
+  (let [drop-one (partial drop 1)
+        item-to-strip (first @list-atom)]
+    (swap! list-atom drop-one)
+    item-to-strip))
+
+(defn undo [& args]
+    ; Drop the current state off the history
+    (-drop-and-return history)
+    ; Now drop the next-last state off the history and return it
+    ; (It'll get added back on)
+    (-drop-and-return history))
+
+(defn print-from-root [loc]
+  (println)
+  (println "The view from root:")
+  (print (z/root loc))
+  (println)
+  )
+
 
 (defn explain [loc]
   ;TODO
@@ -82,7 +113,10 @@
    \a append
    \w wrap
 
+   \h print-history
+   \t print-from-root
    \! new-zipper
+   \z undo
    \? help
    \q exit})
 
@@ -108,6 +142,9 @@
       (let [input (char (.readCharacter term System/in))
             next-fn (interpret input)
             next-zip (next-fn zipper)]
+        (when next-zip
+          (do
+            (swap! history conj next-zip)))
         (recur
           (or         ; action fns either
             next-zip  ; return the modified zipper or return nil,
