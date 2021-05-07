6f265879a526ca43df803c9ce8ccea04173f1587
Egg Syntax <egg@novonon.com>
2021-05-07 15:15:40 -0400

    Remove old broken cider-nrepl

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [jline "0.9.94"]]
-  :plugins [[cider/cider-nrepl "0.8.2"]]
+  ;; :plugins [[cider/cider-nrepl "0.8.2"]]
   :main ^:skip-aot vuet.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
